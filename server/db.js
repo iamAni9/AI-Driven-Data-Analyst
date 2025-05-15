162e90ec5c15f43b80702c889050c622bbdbc52f
Aniket Chaudhary <iamani7464@gmail.com>
2025-05-15 23:04:14 +0530

    Updated db.js for hosted db

--- server/db.js
+++ server/db.js
@@ -2,11 +2,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  // host: process.env.PG_HOST,
+  // port: process.env.PG_PORT,
+  // user: process.env.PG_USER,
+  // password: process.env.PG_PASSWORD,
+  // database: process.env.PG_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
